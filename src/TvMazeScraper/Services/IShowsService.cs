10d11c440d7bb8155d239b078baadbb8dd19c436
Bartek <bartosz.ladzinski@gmail.com>
2020-10-26 15:31:10 +0100

    Add IShowsService

---
+++ src/TvMazeScraper/Services/IShowsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TvMazeScraper.Models;
+
+namespace TvMazeScraper.Services
+{
+    public interface IShowsService
+    {
+        Task<IEnumerable<Show>> GetAsync(int page = 0);
+    }
+}
