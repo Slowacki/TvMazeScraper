49d83e783420dc760f757bc5f15ae09757858dcc
Bartek <bartosz.ladzinski@gmail.com>
2020-10-26 17:10:31 +0100

    Add Dtos

---
+++ src/TvMazeScraper.Web/Dtos/ShowDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace TvMazeScraper.Web.Dtos
+{
+    public class ShowDto
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<PersonDto> Cast { get; set; }
+
+        [IgnoreDataMember]
+        public DateTime CreatedAt { get; set; }
+    }
+}
