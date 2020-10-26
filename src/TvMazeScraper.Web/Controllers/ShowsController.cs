5a59a9eb416c49e2f2fd2624915c7e3e6ce179b7
Bartek <bartosz.ladzinski@gmail.com>
2020-10-26 16:13:16 +0100

    Add ShowsController

---
+++ src/TvMazeScraper.Web/Controllers/ShowsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TvMazeScraper.Models;
+using TvMazeScraper.Services;
+
+namespace TvMazeScraper.Web.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ShowsController : ControllerBase
+    {
+        private readonly IShowsService _showsService;
+
+        public ShowsController(IShowsService showsService)
+        {
+            _showsService = showsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Show>>> Get([FromQuery] int page = 0)
+        {
+            var shows = await _showsService.GetAsync(page);
+
+            return shows.ToList();
+        }
+    }
+}
