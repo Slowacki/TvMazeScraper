551b1e440c0050a717d10d4a48edd7d63ccac076
Bartek <bartosz.ladzinski@gmail.com>
2020-10-26 15:30:58 +0100

    Add shows repository interface

---
+++ src/TvMazeScraper/Repositories/IShowsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TvMazeScraper.Models;
+
+namespace TvMazeScraper.Repositories
+{
+    public interface IShowsRepository
+    {
+        Task<IEnumerable<Show>> GetAsync(int page = 0);
+
+        Task InsertAsync(IEnumerable<Show> shows);
+    }
+}
